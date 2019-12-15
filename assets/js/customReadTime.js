1c3ef43425ac01683db3921c7e352955c959ccc5
dev-prakhar <prakhars1996@gmail.com>
2019-12-16 00:18:29 +0530

    Added Reading Time on Blog Home | Removed Redundant Files

---
+++ assets/js/customReadTime.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.hidden-post-url').each(function () {
+    $('.reading-time-home-ft-' + this.innerText).text(readingTimeCustom($('.hidden-post-content-ft-' + this.innerText).text()))
+    $('.reading-time-home-reg-' + this.innerText).text(readingTimeCustom($('.hidden-post-content-reg-' + this.innerText).text()))
+  })
+})
+
+readingTimeCustom = function(content, wordPerMinute = 200) {
+  var readMinutes = content.split(' ').length / wordPerMinute;
+  return readMinutes > 1 ? " " + Math.round(readMinutes) + ' minute read': " " + Math.round(readMinutes * 60) + ' second read'
+};
